ba258bcfb60de9f0efb95c098a9a40f8d38383e4
Anna Polevina <annapolevina@gmail.com>
2022-01-25 15:33:14 +0300

    Initial Commit

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Anna Polevina on 25.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
